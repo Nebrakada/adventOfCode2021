24fbbc5a9f92456d6302079fb8e51d33f78b3bb3
Kamil Niedzielak <kamil.niedzielak.ctr@sabre.com>
2022-01-11 16:01:36 +0100

    day 4

--- src/test/java/com/adventofcode/app/day4/Board.java
+++ src/test/java/com/adventofcode/app/day4/Board.java
@@ -1,7 +1,9 @@
 package com.adventofcode.app.day4;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import lombok.AllArgsConstructor;
 
@@ -25,6 +27,55 @@ public class Board {
         return "Board{" +
                 "cells=" + Arrays.toString(cells) +
                 '}';
+    }
+
+    public void markNumberIfExists(int number) {
+        for (BoardCell[] row : cells) {
+            for (BoardCell boardCell : row) {
+                if (boardCell.value == number) {
+                    boardCell.isMarked = true;
+                }
+            }
+        }
+    }
+
+    public boolean isGameFinished() {
+        for (BoardCell[] cell : cells) {
+            if (isRowFilled(cell)) {
+                return true;
+            }
+        }
+
+        Map<Integer, List<BoardCell>> columnMap = getColumnMap();
+        return columnMap.values()
+                .stream().anyMatch(this::isColumnFilled);
+    }
+
+    private Map<Integer, List<BoardCell>> getColumnMap() {
+        Map<Integer, List<BoardCell>> columnMap = new HashMap<>();
+        for (BoardCell[] cell : cells) {
+            for (int col = 0; col < cell.length; col++) {
+                final BoardCell boardCell = cell[col];
+                columnMap.compute(col, (key, value) -> {
+                    if (value == null) {
+                        return List.of(boardCell);
+                    } else {
+                        value.add(boardCell);
+                        return value;
+                    }
+                });
+            }
+        }
+        return columnMap;
+    }
+
+    boolean isColumnFilled(List<BoardCell> columns) {
+            return columns.stream().allMatch(cell -> cell.isMarked);
+    }
+
+    boolean isRowFilled(BoardCell[] boardRow) {
+        return Arrays.stream(boardRow)
+                .allMatch(cell -> cell.isMarked);
     }
 
     public int countUnmarkedNumbersSum() {
--- src/test/java/com/adventofcode/app/day4/Day4Test.java
+++ src/test/java/com/adventofcode/app/day4/Day4Test.java
@@ -1,7 +1,9 @@
 package com.adventofcode.app.day4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +14,11 @@ public class Day4Test {
     @Test
     void name() {
         List<String> data = TestData.getData4();
-        String[] digits = data.get(0).split(",");
+
+        List<Integer> digits = Arrays.stream(data.get(0).split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
         Board board;
         List<Board> boards = new ArrayList<>();
 
@@ -29,9 +35,23 @@ public class Day4Test {
         System.out.println("boards: " + boards);
 
         // start game - for every input number mark value if exists in board
+        markSelectedNumbers(digits, boards);
 
         // check if game is finished
 
+
         // if finished, count end result
+    }
+
+    private void markSelectedNumbers(List<Integer> digits, List<Board> boards) {
+        for (Integer digit : digits) {
+            for (Board board : boards) {
+                board.markNumberIfExists(digit);
+                if (board.isGameFinished()) {
+                    System.out.println("\nWinning board\n" + board);
+                    System.out.println(digit * board.countUnmarkedNumbersSum());
+                }
+            }
+        }
     }
 }
